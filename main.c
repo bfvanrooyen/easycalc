48fada46a28a7b7dd4f15f34871bfd39c410e5ad
bfvanrooyen <bf.v.rooyen@barendict.com>
2015-11-09 13:02:40 +0000

    Read input to form expression

--- main.c
+++ main.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 
+int string_contains_q(char *expression);
+
 int main()
 {
-  puts("Enter an expression to be evaluated:");
+	char expression[50];
+
+	puts("Enter an expression to be evaluated:");
+
+	while (string_contains_q(expression) == 0){
+		scanf("%s", expression);
+	}
+}
+
+int string_contains_q(char *expression)
+{
+	int i;
+
+	for (i = 0; i < 50; i++){
+		if (expression[i] == 'q'
+		|| expression[i] == 'Q')
+			return 1;
+
+	}
+
+	return 0;
 }
